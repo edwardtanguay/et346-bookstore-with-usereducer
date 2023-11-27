2bfda736c9aa3739d48ffe8b24c7e11254cbc6d7
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-28 00:58:25 +0100

    books in

--- src/AppContext.tsx
+++ src/AppContext.tsx
@@ -5,6 +5,12 @@ import { useReducer } from "react";
 
 interface IState {
 	userName: string;
+	books: IBook[];
+}
+
+const initialState: IState = {
+	userName: '',
+	books: []
 }
 
 interface IUserNameAction {
@@ -12,15 +18,19 @@ interface IUserNameAction {
 	payload: string;
 }
 
-const initialState: IState = {
-	userName: ''
+interface IBooksAction {
+	type: "setBooks"
+	payload: IBook[];
 }
 
-const reducer = (state: IState, action: IUserNameAction) => {
+const reducer = (state: IState, action: IUserNameAction | IBooksAction) => {
 	const _state = structuredClone(state);
 	switch (action.type) {
 		case "changeUserName":
 			_state.userName = action.payload;
+			break;
+		case "setBooks":
+			_state.books = action.payload;
 			break;
 	}
 	return _state;
@@ -29,8 +39,6 @@ const reducer = (state: IState, action: IUserNameAction) => {
 interface IAppContext {
 	state: IState,
 	dispatch: React.Dispatch<IUserNameAction>,
-	books: IBook[];
-	setBooks: (books: IBook[]) => void;
 	cart: ICart;
 	handleAddBookToCart: (book: IBook) => void;
 }
@@ -45,14 +53,13 @@ export const AppContext = createContext<IAppContext>({} as IAppContext);
 
 export const AppProvider: React.FC<IAppProvider> = ({ children }) => {
 	const [state, dispatch] = useReducer(reducer, initialState);
-	const [books, setBooks] = useState<IBook[]>([]);
 	const [cart, setCart] = useState<ICart>({ items: [] } as ICart);
 
 	useEffect(() => {
 		setTimeout(async () => {
 			const response = await axios.get(booksUrl);
 			const _books = response.data;
-			setBooks(_books);
+			dispatch({type: 'setBooks', payload: _books})
 		}, 2000);
 	}, []);
 
@@ -67,8 +74,6 @@ export const AppProvider: React.FC<IAppProvider> = ({ children }) => {
 			value={{
 				state,
 				dispatch,
-				books,
-				setBooks,
 				cart,
 				handleAddBookToCart,
 			}}
