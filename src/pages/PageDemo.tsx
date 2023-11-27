ea9e0cc55a799c4cdf994a15b4673593f571cb38
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-27 22:28:29 +0100

    demo in

--- src/pages/PageDemo.tsx
+++ src/pages/PageDemo.tsx
@@ -1,5 +1,46 @@
-export const PageDemo = () => {
-	return (
-		<p>this is the demo page</p>
-	)
+import { useReducer } from "react";
+
+interface IState {
+	count: number;
 }
+
+interface IAction {
+	type: "increment" | "decrement";
+	payload: number;
+}
+
+const initialState: IState = {
+	count: 0,
+};
+
+const reducer = (state: IState, action: IAction) => {
+	const _state = structuredClone(state);
+	switch (action.type) {
+		case "increment":
+			_state.count += action.payload;
+			break;
+		case "decrement":
+			_state.count -= action.payload;
+			break;
+	}
+	return _state;
+};
+
+export const PageDemo = () => {
+	const [state, dispatch] = useReducer(reducer, initialState);
+
+	return (
+		<>
+			<h2 className="text-2xl mb-5 font-bold">Demo of useReducer</h2>
+			<div className="flex gap-3">
+				<button onClick={() => dispatch({type: 'decrement', payload: 1})} className="text-4xl pt-0 w-10 h-10 flex items-center justify-center">
+					-
+				</button>
+				<button onClick={() => dispatch({type: 'increment', payload: 1})} className="text-4xl pt-0 w-10 h-10 flex items-center justify-center">
+					+
+				</button>
+				<p className="text-2xl mb-3">Count: {state.count}</p>
+			</div>
+		</>
+	);
+};
