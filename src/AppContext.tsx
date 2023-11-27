69a9eda1809a6f3526634dbcd05260e3fa7b891e
Edward Tanguay (windows) <edward@tanguay.info>
2023-11-28 00:49:33 +0100

    refactor base in

--- src/AppContext.tsx
+++ src/AppContext.tsx
@@ -1,10 +1,34 @@
 import { createContext, useEffect, useState } from "react";
 import { IBook, ICart } from "./interfaces";
 import axios from "axios";
+import { useReducer } from "react";
+
+interface IState {
+	userName: string;
+}
+
+interface IUserNameAction {
+	type: "changeUserName"
+	payload: string;
+}
+
+const initialState: IState = {
+	userName: ''
+}
+
+const reducer = (state: IState, action: IUserNameAction) => {
+	const _state = structuredClone(state);
+	switch (action.type) {
+		case "changeUserName":
+			_state.userName = action.payload;
+			break;
+	}
+	return _state;
+};
 
 interface IAppContext {
-	userName: string;
-	setUserName: (userName: string) => void;
+	state: IState,
+	dispatch: React.Dispatch<IUserNameAction>,
 	books: IBook[];
 	setBooks: (books: IBook[]) => void;
 	cart: ICart;
@@ -20,7 +44,7 @@ const booksUrl = "https://edwardtanguay.vercel.app/share/techBooks.json";
 export const AppContext = createContext<IAppContext>({} as IAppContext);
 
 export const AppProvider: React.FC<IAppProvider> = ({ children }) => {
-	const [userName, setUserName] = useState("");
+	const [state, dispatch] = useReducer(reducer, initialState);
 	const [books, setBooks] = useState<IBook[]>([]);
 	const [cart, setCart] = useState<ICart>({ items: [] } as ICart);
 
@@ -41,8 +65,8 @@ export const AppProvider: React.FC<IAppProvider> = ({ children }) => {
 	return (
 		<AppContext.Provider
 			value={{
-				userName,
-				setUserName,
+				state,
+				dispatch,
 				books,
 				setBooks,
 				cart,
